6492649e854ffd3336682dc65e0ed51be1736ff2
Distant22 <sindistant@gmail.com>
2022-12-01 22:38:30 +0800

    合併topbar跟sidebar

---
+++ src/Components/Pages/SettingPage/SettingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SettingPage(){
+    return(
+        <div className="setting">
+            <h1>設定</h1>
+        </div>
+    );
+}
+
+export default SettingPage;
