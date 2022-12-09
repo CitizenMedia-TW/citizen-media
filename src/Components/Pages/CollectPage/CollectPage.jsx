ede3df7e6c9cc2f84a91861763eaaa705aca747c
timsu92 <33785401+timsu92@users.noreply.github.com>
2022-12-09 16:43:13 +0800

    feat(collect): place a temporary image in all collected articles

--- src/Components/Pages/CollectPage/CollectPage.jsx
+++ src/Components/Pages/CollectPage/CollectPage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import "./CollectPage.css"
 import { CollectPageData } from './CollectPageData';
 import { Link } from 'react-router-dom';
+import "../../Layout/Sidebar/Sidebar.css"
+import LogoNoBG from "../../../Assets/LogoNoBG.png"
 
 function CollectPage(){
     return(
@@ -12,6 +14,7 @@ function CollectPage(){
                         <Link style={{textDecoration: 'none'}} to = {item.path} className = "collectBlock">
                             <div className="collectText">
                                 <span>{item.title}</span>
+								<img className="logo" src={LogoNoBG}></img>
                             </div> 
                         </Link>
                     )
