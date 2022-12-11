bda0e9c55fb4fe618ba092709564acc905ee1d7a
LightWat <dennislai120@gmail.com>
2022-12-11 22:02:58 +0800

    fix: css小修正
    impact: low

--- src/Components/Pages/HomePage/Carousel/NewsBlock.js
+++ src/Components/Pages/HomePage/Carousel/NewsBlock.js
@@ -5,16 +5,14 @@ const NewsBlock = ({newsDetail}) => {
   const {title, description, img} = newsDetail;
 
   return (
-    <div className="item">
-      <div className="news_child">
-        <p className="news_title">{title}</p>
-        <img className="news_img" src={img} alt="" />
-        <p className="news_info">{description}</p>
+    <div className="news_child">
+      <p className="news_title">{title}</p>
+      <img className="news_img" src={img} alt="" />
+      <p className="news_info">{description}</p>
 
-        <a href="https://google.com" target="_blank" rel="noreferrer">
-          <button className="button_more_info">更多內容...</button>
-        </a>
-      </div>
+      <a href="https://google.com" target="_blank" rel="noreferrer">
+        <button className="button_more_info">更多內容...</button>
+      </a>
     </div>
   );
 };
