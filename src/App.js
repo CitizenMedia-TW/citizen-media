9dec6431d1fd5fbffe16349a24d1da4e90d7caf9
GINTOAHC <chaotingchen10@gmail.com>
2022-12-20 14:32:37 +0800

    style(App.js): Remove unused useParamas & Change popular page routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route, useParams  } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Topbar from './Components/Layout/Topbar/Topbar'
 import HomePage from './Components/Pages/HomePage/HomePage'
 import LoginPage from './Components/Pages/LoginPage/LoginPage'
-import PoliticsPage from './Components/Pages/PoliticsPage/PoliticsPage'
+import PopularPage from "./Components/Pages/PopularPage/PopularPage";
+/* import PoliticsPage from './Components/Pages/PoliticsPage/PoliticsPage' */
 
 import NotifyPage from "./Components/Pages/NotifyPage/NotifyPage";
 import SettingPage from "./Components/Pages/SettingPage/SettingPage";
 import DraftPage from "./Components/Pages/DraftPage/DraftPage";
 import CollectPage from "./Components/Pages/CollectPage/CollectPage";
-import PopularPage from "./Components/Pages/PopularPage/PopularPage";
 import NewsPage from "./Components/Pages/NewsPage/NewsPage";
 
 const App = () => {
@@ -18,14 +18,15 @@ const App = () => {
     <Router>
         <Topbar />
         <Routes>
-            <Route exact path="/" element={ <HomePage /> } />
-            <Route exact path="/login" element={ <LoginPage /> } />
-            <Route exact path="/politics" element={ <PopularPage /> } />
-            <Route exact path="/notify" element={ <NotifyPage /> } />
-            <Route exact path="/setting" element={ <SettingPage /> } />
-            <Route exact path="/draft" element={ <DraftPage /> } />
-            <Route exact path="/collect" element={ <CollectPage /> } />
-            <Route exact path="/news/:id" element={<NewsPage/>} />
+          <Route exact path="/" element={ <HomePage /> } />
+          <Route exact path="/login" element={ <LoginPage /> } />
+          <Route exact path="/popular" element={ <PopularPage /> } />
+          {/* SideBar */}
+          <Route exact path="/notify" element={ <NotifyPage /> } />
+          <Route exact path="/setting" element={ <SettingPage /> } />
+          <Route exact path="/draft" element={ <DraftPage /> } />
+          <Route exact path="/collect" element={ <CollectPage /> } />
+          <Route exact path="/news/:id" element={ <NewsPage/> } />
         </Routes>
     </Router>
   );
