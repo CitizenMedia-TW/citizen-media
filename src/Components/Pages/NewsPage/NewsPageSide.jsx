55473e87a32f1554b65badfd154fe1fa90eb3f42
Distant22 <sindistant@gmail.com>
2022-12-24 12:45:42 +0800

    更新新聞頁背景色

--- src/Components/Pages/NewsPage/NewsPageSide.jsx
+++ src/Components/Pages/NewsPage/NewsPageSide.jsx
@@ -11,7 +11,7 @@ const NewsPageSide = () => {
   const {id} = useParams();
 
   // 用Array儲存目前新聞數，之後須改成動態捕捉新聞數量並更新Array
-  var arr = [0, 1, 2, 3];
+  var arr = [0, 1, 2, 3, 4];
   for( var i = 0; i < arr.length; i++){ 
       // 讓旁邊feature去掉目前的新聞
       if ( arr[i] === id-1) { 
@@ -20,10 +20,21 @@ const NewsPageSide = () => {
   }
 
   return (
-    <div className="news_page_side_container">
-
+    <div>
+      <p className="news_page_feature">Feature</p>
+      <div className="news_page_side_container">
         {/* 用index取得資料 */}
-        <Link to = {`/news/${news[arr[0]]["id"]}`} >
+
+        {arr.map(num => {
+          return (
+            <Link to = {`/news/${news[num]["id"]}`} >
+              <p className="news_side_title">{news[num]["title"]}</p>
+              <img className="news_side_img" src={news[num]["img"]} alt="" />
+            </Link>
+          )
+        })}
+
+        {/* <Link to = {`/news/${news[arr[0]]["id"]}`} >
           <p className="news_side_title">{news[arr[0]]["title"]}</p>
           <img className="news_side_img" src={news[arr[0]]["img"]} alt="" />
         </Link>
@@ -36,8 +47,10 @@ const NewsPageSide = () => {
         <Link to = {`/news/${news[arr[2]]["id"]}`} >
           <p className="news_side_title">{news[arr[2]]["title"]}</p>
           <img className="news_side_img" src={news[arr[2]]["img"]} alt="" />
-        </Link>
+        </Link> */}
+      </div>
     </div>
+    
   );
 };
 
