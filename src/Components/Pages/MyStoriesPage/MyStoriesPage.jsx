242fa22f0607fe4c8354f81df2874fdbc0cea36f
GNITOAHC <chaotingchen10@gmail.com>
2023-02-17 00:22:09 +0800

    feat(stories): List out stories title to page

--- src/Components/Pages/MyStoriesPage/MyStoriesPage.jsx
+++ src/Components/Pages/MyStoriesPage/MyStoriesPage.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import ProfileService from "../../../services/profile.service";
 
 const MyStoriesPage = ({ currentUser }) => {
+  let [myStories, setMyStories] = useState([]);
+  useEffect(() => {
+    if (currentUser) {
+      ProfileService.myStories()
+        .then((data) => {
+          setMyStories(data.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+  }, [currentUser]);
+
   return (
     <div>
       {!currentUser && <Link to="/login">Login First</Link>}
       {currentUser && (
         <div>
-          MyStoriesPage
           <Link to="/stories/draft">Draft</Link>
+          <div>
+            Titles of stories:
+            {myStories.map((item, index) => {
+              return (
+                <li key={index} style={{ listStyle: "none" }}>
+                  <p>{item.title}</p>
+                </li>
+              );
+            })}
+          </div>
         </div>
       )}
     </div>
