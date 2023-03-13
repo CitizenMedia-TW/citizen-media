0ea4e250d3bb6924995b63e143f61c4b167fe055
GINTOAHC <chaotingchen10@gmail.com>
2023-03-13 15:49:32 +0800

    perf: 更改 async function 和增加 login 的catch error

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,23 +2,26 @@ import axios from "axios";
 const API_URL = "http://localhost:8080/api/user";
 
 class AuthService {
-  login(email, password) {
-    return axios.post(API_URL + "/login", { email, password });
+  async login(email, password) {
+    return axios.post(API_URL + "/login", { email, password }).catch((err) => {
+      throw err;
+    });
   }
 
   logout() {
     localStorage.removeItem("user");
   }
 
-  register(username, email, password) {
-    return axios.post(API_URL + "/register", {
-      username,
-      email,
-      password,
-    })
-    .catch((err) => {
-      throw err;
-    });
+  async register(username, email, password) {
+    return axios
+      .post(API_URL + "/register", {
+        username,
+        email,
+        password,
+      })
+      .catch((err) => {
+        throw err;
+      });
   }
 
   getCurrentUser() {
