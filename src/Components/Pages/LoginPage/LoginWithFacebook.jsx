0e5bc3d101aadcf4085c5ebb00de70cad68c3ef3
raamiiChu <ericchu0617@gmail.com>
2023-04-22 17:00:30 +0800

    feat: Add Facebook login button

--- src/Components/Pages/LoginPage/LoginWithFacebook.jsx
+++ src/Components/Pages/LoginPage/LoginWithFacebook.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import FacebookLogin from "react-facebook-login";
+import FacebookLoginIcon from "../../../Assets/FacebookLoginIcon.png"
+
 
 import { Facebook_Client_Id } from './Client_Id';
 
@@ -10,10 +12,21 @@ const LoginWithFacebook = () => {
     
     return (
         <FacebookLogin
+            cssClass = "px-20 h-12 flex items-center text-base	 font-serif font-medium text-white rounded-4xl bg-[#0f3e7a]"
+            
             appId = {Facebook_Client_Id}
-            autoLoad = {false}
+            language = 'en_US'
+            textButton = "Continue with Facebook"
             fields = "name, email, picture"
+            autoLoad = {false}            
             callback = {responseFacebook} 
+
+            // FB login icon 
+            icon = {<img 
+                className = 'scale-75 mr-2 ml-6' 
+                src = {FacebookLoginIcon} 
+                alt = "Facebook Login Icon"
+            />}
         />
     );
 };
