108b35bcbd9893a797fa85324f4c13005b2d444a
Alex <suc1044080333@gmail.com>
2023-05-01 17:04:38 +0800

    fix: 將class改成className

--- src/Components/Layout/Sidebar/Sidebar.jsx
+++ src/Components/Layout/Sidebar/Sidebar.jsx
@@ -15,30 +15,30 @@ const Sidebar = () => {
 
   return (
     <>
-        <div class='ml-1 mr-4 text-xl bg-none text-[#426CA3]'>
+        <div className='ml-1 mr-4 text-xl bg-none text-[#426CA3]'>
             <Link to = "#">
                 <FaIcons.FaBars onMouseEnter={toggleSidebar}/>
             </Link>
         </div>
-        <nav class={sidebar ? 'bg-[#426CA3]  border-r-2 border-solid w-[270px] h-[100vh] flex justify-center fixed top-0 right-0 transition-[200ms] z-0' : 'bg-[#426CA3] border-r-2 border-solid w-[270px] h- flex justify-center fixed top-0 right-[-270px] transition-[200ms]'}>
-            <div class='absolute top-0 w-full h-16 bg-[#426CA3]'>
-                <input type='button' class="bg-[url('/src/Assets/Close.png')] w-10 h-1/6 ml-56 mt-3 pt-6"></input>
+        <nav className={sidebar ? 'bg-[#426CA3]  border-r-2 border-solid w-[270px] h-[100vh] flex justify-center fixed top-0 right-0 transition-[200ms] ' : 'bg-[#426CA3] border-r-2 border-solid w-[270px] h- flex justify-center fixed top-0 right-[-270px] transition-[200ms] '}>
+            <div className='absolute top-0 w-full h-16 bg-[#426CA3]'>
+                <input type='button' className="bg-[url('/src/Assets/Close.png')] w-10 h-1/6 ml-56 mt-3 pt-6"></input>
             </div>
             <ul className = "w-full" onMouseLeave={toggleSidebar}>       
-                <div class= 'h-[100vh_-_250px] bottom-0 z-[500] flex flex-col overflow-y-auto border-b-0 mt-[75%] ml-[5%] mr-[12%]'>
+                <div className= 'h-[100vh_-_250px] bottom-0 z-[500] flex flex-col overflow-y-auto border-b-0 mt-[75%] ml-[5%] mr-[12%]'>
                     {SidebarData.map((item,index) => {
                     return (
                             <li key={index} className={item.cName}>
                                 <Link to = {item.path}>
-                                    <span>{item.title}<div class='w-32 mb-1 mt-1 flex justify-center items-center border-t-[2px] border-solid border-t-line-border-bar bg-line-border-bar'></div></span>
+                                    <span>{item.title}<div className='w-32 mb-1 mt-1 flex justify-center items-center border-t-[2px] border-solid border-t-line-border-bar bg-line-border-bar'></div></span>
                                 </Link>
                             </li>
                         )
                     })} 
                 </div>   
             </ul> 
-            <div class='absolute bottom-0 w-full h-[70px] bg-[#426CA3]'>
-            <img src={Logo} class='flex w-1/2 ml-[40%] pt-[5%]'></img>
+            <div className='absolute bottom-0 w-full h-[70px] bg-[#426CA3]'>
+            <img src={Logo} className='flex w-1/2 ml-[40%] pt-[5%]'></img>
             </div>
         </nav>
     </>
