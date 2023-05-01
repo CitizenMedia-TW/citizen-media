ee95f3f2e029d5e9b084c53775318eaa2356283d
Distant22 <sindistant@gmail.com>
2023-05-01 22:08:32 +0800

    fix(sidenews):adjust css and overflow to side news

--- src/Components/Pages/HomePage/SideNews/SideNews.jsx
+++ src/Components/Pages/HomePage/SideNews/SideNews.jsx
@@ -7,17 +7,24 @@ const SideNews = () => {
             <div className="mb-5 mt-5 ml-5 text-gray-500">
                 Recommanded Topic
             </div>
-            <div className="flex">
-                <div className="rounded-3xl text-center h-[2rem] bg-gray-300 px-3 mx-5">
-                    Machine Learning
+            <div className="grid grid-cols-2 gap-4 ml-4">
+                <div className=" rounded-3xl px-2 flex items-center justify-center h-[2rem] w-[11rem] bg-gray-300">
+                    <p>Machine Learning</p>
                 </div>
-                <div className="rounded-3xl text-center h-[2rem] w-[8rem] bg-gray-300">
-                    
+                <div className="invisible md:visible rounded-3xl px-2 flex items-center h-[2rem] w-[8rem] bg-gray-300">
                 </div>
-                {/* <div className="rounded-3xl text-center h-[2rem] bg-gray-300">
-                    Writing
-                </div> */}
+                <div className=" rounded-3xl px-2 flex items-center justify-center h-[2rem] w-[6rem] bg-gray-300">
+                    <p>Writing</p>
+                </div>
+                <div className="invisible md:visible rounded-3xl px-2 flex items-center h-[2rem] w-[8rem] bg-gray-300 -translate-x-20">
+                </div>
+                <div className=" rounded-3xl px-2 flex items-center justify-center h-[2rem] w-[8rem] bg-gray-300">
+                    <p>Phychology</p>
+                </div>
+                <div className="invisible md:visible rounded-3xl px-2 flex items-center h-[2rem] w-[8rem] bg-gray-300 -translate-x-12">
+                </div>
             </div>
+            
         </div>
     )
 }
