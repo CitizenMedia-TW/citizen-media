b434039d01007eefdeb317a6717a08c4f55e6481
Distant22 <sindistant@gmail.com>
2023-05-01 22:09:06 +0800

    fix(sidescrollnews):adjust css and handle overflow to side scroll news

--- src/Components/Pages/HomePage/SideScrollNews/SideScrollNews.js
+++ src/Components/Pages/HomePage/SideScrollNews/SideScrollNews.js
@@ -13,24 +13,26 @@ const SideScrollNews = () => {
                 The latest news
             </div>
             {NewsData.map((item) => {
-                    return (
-                        <Link style={{textDecoration: 'none'}} to = {`/news/${item.id}`} className = "">
-                            <div className="ml-8 grid grid-cols-2 h-[8rem] mb-8">
-                                <div className="h-full">
-                                    <div className="flex mb-3 font-bold">
-                                        <img className="rounded-full w-[2rem] h-[2rem] ml-8" src={item.banner} alt="logo"></img>
-                                        <span>{item.author}</span>
-                                    </div>
-                                    <span className="font-extrabold">{item.title}</span>
-                                    <br />
-                                    <div className="text-sm tracking-wide align-bottom align-bottom h-full align-text-bottom">{item.time[1]} {item.time[2]} ‧ 4 min read ‧ Medium</div>
+                return (
+                    <Link style={{textDecoration: 'none'}} to = {`/news/${item.id}`} className = "">
+                        <div className="ml-8 grid grid-cols-2 h-[8rem] mb-8">
+                            <div className="h-full">
+                                <div className="flex font-bold items-center">
+                                    <img className="rounded-full w-[2rem] h-[2rem]" src={item.banner} alt="logo"></img>
+                                    <p className="ml-3 line-clamp-1">{item.author}</p>
                                 </div>
-                                <div>
-                                    <img className="h-[8rem] w-[16rem]" src={item.img} alt={item.title} />
+                                <p className="font-extrabold mt-2 line-clamp-2">{item.title}</p>
+                                
+                                <div className="text-sm flex items-end">
+                                    <p className="line-clamp-1">{item.time[1]} {item.time[2]} ‧ 4 min read ‧ Medium</p>
                                 </div>
-                            </div> 
-                        </Link>
-                    )
+                            </div>
+                            <div className="flex">
+                                <img className="invisible md:visible md:w-[10rem] lg:visible h-[8rem] lg:w-[12rem] mx-4" src={item.img} alt={item.title} />
+                            </div>
+                        </div> 
+                    </Link>
+                )
             })}       
         </div>
     )
